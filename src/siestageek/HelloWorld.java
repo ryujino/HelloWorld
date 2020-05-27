b6e0b1a298b187d0bdb0586886606e1956a7f4a9
jinoru <64898455+ryujino@users.noreply.github.com>
2020-05-27 15:06:57 +0900

    얍

--- src/siestageek/HelloWorld.java
+++ src/siestageek/HelloWorld.java
@@ -3,7 +3,7 @@ package siestageek;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!!");
+        System.out.println("야호, World!!");
     }
 
 }
